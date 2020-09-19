216fa58171a8a97614a55c1f2974b31088d78523
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-09-19 11:53:19 +0200

    I am  adding the specs for the DisplayCooperResult component (that is not yet created), in src/__tests__/DisplayCooperResult.spec.js, I am then creating the component for the cooper result as a functional component in DisplayCooperResult.jsx. I am then creating the  cooperCalculator functionality (for calculations) as a js module in cooperCalculator.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import DisplayCooperResult from "./components/DisplayCooperResult";
 
 class App extends Component {
-	render() {
-		return(
-			<>
-			
+  state = {
+    distance: "",
+    gender:"", 
+  }
 
-			</>
-		);
-	}
+
+
+  render() {
+    return (
+      <>
+        <label>Distance</label>
+        <input name="distance" id="distance"></input>
+
+        <select name="gender" id="gender">
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+
+        <label>Age</label>
+        <input name="age" id="age"></input>
+      </>
+    );
+  }
 }
 
 export default App;
